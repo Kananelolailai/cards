4773d9e7ee5fa1be2c3ccdcdde597e191617b5c9
Kananelolailai <malefek@gmail.com>
2025-07-05 20:50:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const cardImage = document.getElementById('card-image');
 
 // Define which image corresponds to which button
 const cardImages = {
-    shoprite: 'shoprite',
+    shoprite: 'shoprite.jpg',
     clicks: 'clicks.jpg',
     pnp: 'pnp.jpg',
     boxer: 'boxer.jpg',
